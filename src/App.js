a38e8940764471c5de1a92fe8a8656d4e9f7b2a8
unknown <prashanthmp009@gmail.com>
2022-05-09 18:00:53 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/messaging';
+import 'firebase/compat/functions';
 
 function App() {
+
+  React.useEffect(()=>{
+    var firebaseConfig = {
+      apiKey: "AIzaSyCSv_tbFCz1-Ij-IlHDaQ0BTbM3G24dbpU",
+      authDomain: "push-notify-feae9.firebaseapp.com",
+      projectId: "push-notify-feae9",
+      storageBucket: "push-notify-feae9.appspot.com",
+      messagingSenderId: "907834086531",
+      appId: "1:907834086531:web:746af194053fdb89cf37b6"
+    };
+    
+    // Initialize Firebase
+    const firebaseApp =firebase.initializeApp(firebaseConfig);
+  
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+    const msg=firebase.messaging()
+    
+    
+       msg.getToken().then((data)=>{
+      console.log("token",data)
+    })
+  })
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/firebaseApp.js
@@ -0,0 +1,14 @@
+// import firebase from 'firebase';
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCSv_tbFCz1-Ij-IlHDaQ0BTbM3G24dbpU",
+//     authDomain: "push-notify-feae9.firebaseapp.com",
+//     projectId: "push-notify-feae9",
+//     storageBucket: "push-notify-feae9.appspot.com",
+//     messagingSenderId: "907834086531",
+//     appId: "1:907834086531:web:746af194053fdb89cf37b6"
+//   };
+  
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+
+// export default op;
